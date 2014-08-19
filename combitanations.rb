463ea9bdf73ca185ea8478d30c8acceb3f6d5d9e
dibikhin <dibikhin@yahoo.com>
2014-08-19 20:14:46 +0400

    bit manipulation introduced ok

--- combitanations.rb
+++ combitanations.rb
@@ -1,22 +1,33 @@
-﻿# for 'abc' combinations will be 
+﻿require 'pp'
+require 'ruby-prof'
+
+# for 'abc' combinations will be 
 #	[['c'], ['b'], ['b', 'c'], ['a'], ['a', 'c'], ['a', 'b'], ['a', 'b', 'c']]
 # due to bits of 1 to 7
 #	001, 010, 011, 100, 101, 110, 111
 # sequentially projected on 'abc'
-def find_combinations(str)
+def find_combinations(source)
+	RubyProf.start
 	combs = []
-	len = 2 ** str.size - 1
+	len = 2 ** source.size - 1
+	num_bin_hash = {}
 	(1..len).each do |size_num|
 		comb, counter = [], 0
-		size_num_str = ("%0" + str.size.to_s + "b") % size_num
-		size_num_str.each_char do |bit|
-			comb << str[counter] if bit == '1'
-			counter += 1
+	    # size_num_str = ("%0" + source.size.to_s + "b") % size_num
+		# puts size_num, size_num_str
+		# size_num_str.each_char do |bit|
+			# comb << source[counter] if bit == '1'
+			# counter += 1
+		# end
+		(0..source.size).each do |bit_num| 
+			comb << source[counter] if size_num & ( 1 << counter ) > 0
+			counter += 1 
 		end
 		combs << comb
 	end
+	puts RubyProf::FlatPrinter.new(RubyProf.stop).print(STDOUT)
 	combs
 end
 
-source = 'abc'
-puts find_combinations(source).inspect
+source = (0..16).to_a
+find_combinations(source)
