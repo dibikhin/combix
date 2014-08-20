567c58d6bf880ee1f51edbe019eed0e9a8604a96
dibikhin <dibikhin@yahoo.com>
2014-08-20 21:40:50 +0400

    limit introduced

--- combitanations.rb
+++ combitanations.rb
@@ -6,28 +6,29 @@ require 'ruby-prof'
 # due to bits of 1 to 7
 #	001, 010, 011, 100, 101, 110, 111
 # sequentially projected on 'abc'
-def find_combinations(source)
+def find_combinations(source, selection_size)
 	RubyProf.start
-	combs = []
+	combinations = []
 	len = 2 ** source.size - 1
 	num_bin_hash = {}
 	(1..len).each do |size_num|
-		comb, counter = [], 0
-	    # size_num_str = ("%0" + source.size.to_s + "b") % size_num
-		# puts size_num, size_num_str
-		# size_num_str.each_char do |bit|
-			# comb << source[counter] if bit == '1'
-			# counter += 1
-		# end
-		(0..source.size).each do |bit_num| 
-			comb << source[counter] if size_num & ( 1 << counter ) > 0
-			counter += 1 
+		selection, counter = [], 0
+		puts ("%0" + source.size.to_s + "b") % size_num
+		bit_counter = 0
+		(0..source.size).each do |bit_num|
+			if size_num & ( 1 << counter ) > 0
+				bit_counter += 1
+				if bit_counter >= selection_size
+					selection << source[counter]				
+				end
+			end
+			counter += 1
 		end
-		combs << comb
+		combinations << selection
 	end
 	puts RubyProf::FlatPrinter.new(RubyProf.stop).print(STDOUT)
-	combs
+	combinations
 end
 
-source = (0..16).to_a
-find_combinations(source)
+source = (0..2).to_a
+pp find_combinations(source, 2)
